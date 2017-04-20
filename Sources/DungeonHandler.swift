0830659d8b7a42fab0df481e8496b9405867c410
Chris Brind <c.brind@waracle.com>
2017-04-20 01:23:27 +0100

    start to flesh out dungeon command approach

--- Sources/DungeonHandler.swift
+++ Sources/DungeonHandler.swift
@@ -1,6 +1,10 @@
 
 class DungeonHandler: CommandHandler {
-    
+
+    let commands = [
+        "quit" : QuitHandler()
+    ]
+
     let user: User
     
     init(user: User) {
@@ -8,13 +12,15 @@ class DungeonHandler: CommandHandler {
     }
     
     func handle(io: TerminalIO, world: World) -> CommandHandler? {
-        log(tag: self, message: "handle IN")
+        log(tag: self, message: "handle IN, commands: \(commands)")
         
-        guard io.print("Welcome to swiftmud. ") else {
+        guard io.print("Welcome to SwiftMud. \n") else {
             log(tag: self, message: "failed to write welcome message")
             return nil
         }
-        
+
+        // io.broadcast(to: world.entryRoom().users, "\(user.name) has materialised.")
+
         while(true) {
             guard io.print("> ") else {
                 log(tag: self, message: "failed to write prompt")
@@ -26,8 +32,16 @@ class DungeonHandler: CommandHandler {
                 return nil
             }
 
-            if line == "quit" {
-                break
+            let command = line.components(separatedBy: " ")[0]
+            log(tag: self, message: "command: \(command)")
+            if let handler = commands[command] {
+                let args = line.substring(from: line.index(line.startIndex, offsetBy: command.utf8.count)).trimmingCharacters(in: .whitespacesAndNewlines)
+                log(tag: self, message: "args: \(args)")
+
+                guard handler.execute(args: args, with: io, in: world) else {
+                    log(tag: self, message: "handler has failed (or quit)")
+                    return nil
+                }
             }
 
             guard io.print("Sorry, I don't know how to '\(line)'\n") else {
@@ -38,7 +52,26 @@ class DungeonHandler: CommandHandler {
         }
         
         log(tag: self, message: "handle OUT")
-        return nil
+        return self
     }
     
+}
+
+protocol DungeonCommandHandler: CustomStringConvertible {
+
+    func execute(args: String, with io: TerminalIO, in world: World) -> Bool
+
+}
+
+class QuitHandler: DungeonCommandHandler {
+
+    var description: String {
+        return "[message] - exit SwiftMud with optional message"
+    }
+
+    func execute(args: String, with io: TerminalIO, in world: World) -> Bool {
+        log(tag: self, message: "User has quit with args: [\(args)]")
+        return false
+    }
+
 }
